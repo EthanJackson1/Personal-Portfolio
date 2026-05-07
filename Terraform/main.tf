e785c82f49056de046e8d130332e3eda3abd6d31
Ethan <ethan.jackson-9@students.plymouth.ac.uk>
2026-05-07 12:10:14 +0100

    Added terraform script to recreate table

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,3 +10,14 @@ terraform {
 provider "aws" {
   region = "eu-west-2" # London - make sure this matches your current setup!
 }
+
+resource "aws_dynamodb_table" "visitor_counter" {
+  name           = "visitor-counter"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
